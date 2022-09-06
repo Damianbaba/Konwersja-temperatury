0bd1153383227939d12d948a57e627cc158ff24c
Damian Mroczek <damianmroczek@yahoo.com>
2022-09-06 12:50:54 +0200

    add js functions

--- main.js
+++ main.js
@@ -14,16 +14,44 @@ const swap = () => {
     if (one.textContent === '°C') {
         one.textContent = '°F'
         two.textContent = '°C'
+        result.textContent = ''
     } else {
         one.textContent = '°C'
         two.textContent = '°F'
+        result.textContent = ''
     }
 }
 
+const fahrToCel = () => {
+    fahrenheit = converter.value * 1.8 + 32
+    result.textContent = `${converter.value}°C to ${fahrenheit.toFixed(1)}°F`
+    converter.value = ''
+}
+
+const celToFahr = () => {
+    celsius = (converter.value - 32) / 1.8
+    result.textContent = `${converter.value}°F to ${celsius.toFixed(1)}°C`
+    converter.value = ''
+}
+
+const conversion = () => {
+    if (converter.value !== '') {
+        if (one.textContent === '°C') {
+            fahrToCel()
+        } else {
+            celToFahr()
+        }
+
+    } else {
+        result.textContent = "Musisz podać jakaś wartość"
+    }
+}
+
+const reset = () => {
+    converter.value = ''
+    result.textContent = ''
+}
+
 changeBtn.addEventListener('click', swap)
-
-
-
-
-
-
+convBtn.addEventListener('click', conversion)
+resetBtnt.addEventListener('click', reset)
